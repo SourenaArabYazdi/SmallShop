0c24161f9f27bdc19ab4e32a31e2dcfde6fd4ffe
Soren <yazdi_sourena@yahoo.com>
2025-07-16 19:07:59 +0330

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,49 @@
 import NextAuth from "next-auth";
-import  CredentialsProvider  from "next-auth/providers/credentials";
-import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
 
-export const handler = NextAuth({
-    providers : [
-      GithubProvider({
-         clientId : process.env.GITHUB_ID as string , 
-         clientSecret : process.env.GITHUB_SECRET as string
-         
-      })
-      
-      , 
+const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
 
-        CredentialsProvider({
-           name : 'Credentials' , 
-           credentials : {
-            email : {label : 'email' , type : 'email'} , 
-            password : {label : 'password' , type :  'password'}
-           } , 
-           async authorize(credentials) { 
-              const user = {id : '1' , name : 'Sourena Yazdi' , email : 'usertest@gmail.com' , password : 'user123123' , image : 'https://avatars.githubusercontent.com/u/168073087?v=4'};
-              if (credentials?.email === user.email && credentials?.password === user.password) return user;
-              return null;
-           }
-        })
-    ] , 
-    pages : {
-        signIn : '/Website/Forms/SignIn'
-    } ,
-     session : {
-        strategy : 'jwt'
-     } , 
-     secret : process.env.NEXTAUTH_SECRET, 
-})
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "1",
+          name: "Sourena Yazdi",
+          email: "usertest@gmail.com",
+          password: "user123123",
+          image: "https://avatars.githubusercontent.com/u/168073087?v=4",
+        };
 
+        if (
+          credentials?.email === user.email &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        }
 
-export {handler as GET , handler as POST}
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/Website/Forms/SignIn",
+  },
+  session: {
+    strategy: "jwt" as const,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
